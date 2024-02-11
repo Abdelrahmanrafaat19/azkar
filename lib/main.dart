54aa44be7562e79847e7e1dd7f1b64a7314ccd79
Abdelrahman Raafat Shoaib <93776528+Abdelrahmanrafaat19@users.noreply.github.com>
2024-02-11 12:02:46 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(MediaQuery.of(context).size.width,
+          MediaQuery.of(context).size.height),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          locale: const Locale('ar', 'AR'), // Set the locale to Arabic
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('ar', 'AR'), // Arabi
+          ],
+          home: child,
+        );
+      },
+      child: const Scaffold(),
+    );
+  }
+}
